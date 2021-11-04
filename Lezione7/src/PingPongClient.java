12398bc8d02f1ee2a71b9fe4dc29f833368214e4
Nfinzki <francescodiluzio07@gmail.com>
2021-11-04 14:57:27 +0100

    Aggiunto testo esercizio

--- Lezione7/src/PingPongClient.java
+++ Lezione7/src/PingPongClient.java
@@ -1,3 +1,12 @@
+/*
+L'esercizio consiste nella scrittura di un server che offre il servizio di "Ping Pong" e del relativo programma
+client.
+Un client invia al server un messaggio di "Ping".
+Il server, se riceve il messaggio, risponde con un messaggio di "Pong".
+Il client sta in attesa n secondi di ricevere il messaggio dal server (timeout) e poi termina.
+Client e Server usano il protocollo UDP per lo scambio di messaggi.
+ */
+
 import java.io.IOException;
 import java.net.*;
 
