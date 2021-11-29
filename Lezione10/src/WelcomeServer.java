bf43a5373ecae95597bd8e06a0590c97df8a0fc4
Nfinzki <francescodiluzio07@gmail.com>
2021-11-29 15:09:45 +0100

    Aggiunto esercizio di preparazione all'assignment

---
+++ Lezione10/src/WelcomeServer.java
@@ -0,0 +1,37 @@
+/*
+Definire un Server WelcomeServer, che
+  - invia su un gruppo di multicast (welcomegroup*), ad intervalli regolari, un messaggio
+di «welcome».
+  - attende tra un invio ed il successivo un intervallo di tempo simulato mediante il
+metodo sleep().
+
+*Ad esempio con indirizzo IP 239.255.1.3
+*/
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class WelcomeServer {
+    private static final int port = 42069;
+    private static final int waitTime = 2000;
+
+    public static void main(String []args) {
+        try (MulticastSocket multicastSocket = new MulticastSocket(port)) {
+            InetAddress groupAddress = InetAddress.getByName(args[0]);
+            byte []welcomeMessage = "<<Welcome!>>".getBytes();
+
+            DatagramPacket packet = new DatagramPacket(welcomeMessage, welcomeMessage.length, groupAddress, port);
+
+            while(true) {
+                System.out.println("Sending " + packet);
+                multicastSocket.send(packet);
+                Thread.sleep(waitTime);
+            }
+        } catch (IOException | InterruptedException e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+    }
+}
